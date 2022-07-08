0996da3e7433f1190aa732875b8b621bda966b62
Raul Barranco <maverick@Rauls-MacBook-Pro.local>
2022-07-08 06:52:03 -0400

    adding all files

---
+++ BasicLayoutApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  BasicLayoutApp
+//
+//  Created by Raul Barranco on 7/8/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // navigation button outlet
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
